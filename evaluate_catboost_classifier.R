74f825c4183df7b1a315b1ea860a91641e61757e
Harpo MAxx <harpomaxx@gmail.com>
2020-10-04 21:45:16 +0000

    add support for up and down sampling

--- evaluate_catboost_classifier.R
+++ evaluate_catboost_classifier.R
@@ -1,12 +1,11 @@
-library(dplyr)
-library(randomForest)
-library(readr)
-library(tibble)
-library(stringr)
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(catboost))
+suppressPackageStartupMessages(library(readr))
+suppressPackageStartupMessages(library(tibble))
+suppressPackageStartupMessages(library(stringr))
 suppressPackageStartupMessages(library("optparse"))
 suppressPackageStartupMessages(library("caret"))
 suppressPackageStartupMessages(library("e1071"))
-
 
 read_valid_csv<-function(data_csv){
   
@@ -62,7 +61,6 @@ dataset_vectorized <- dataset_vectorized %>% mutate(size_m = (size_m / modelsize
 dataset_vectorized <- dataset_vectorized %>% mutate(size_l = (size_l / modelsize))
 
 #Making feature vectors
-
 dataset_vectorized <- dataset_vectorized %>% select('strong_p','weak_p','weak_np','strong_np','duration_s',
                                                     'duration_m','duration_l','size_s','size_m','size_l','modelsize','class','label')
 
@@ -70,11 +68,10 @@ names(dataset_vectorized) <- c("sp","wp","wnp","snp","ds","dm","dl","ss","sm","s
 
 
 #dataset_vectorized<-dataset_vectorized %>% mutate(class=ifelse(grepl(pattern = "Normal", x = label),0,1))
-dataset_vectorized$class<-as.factor(dataset_vectorized$class)
+#dataset_vectorized$class<-as.factor(dataset_vectorized$class)
 #
 dataset_vectorized %>% group_by(class) %>% summarize(tot=n())
 dataset_vectorized
-
 }
 
 train_test_sample<-function(x,percent=0.7){
@@ -84,9 +81,10 @@ train_test_sample<-function(x,percent=0.7){
 }
 
 ### Function Definitions ####
-get_predictions <- function(model, test_dataset_x,threshold=0.5) {
-  predsprobs<- predict(model,test_dataset_x,type='prob')
-  preds<-ifelse(predsprobs[,1]>0.5,0,1)
+get_predictions <- function(model, pool_test,threshold=0.5) {
+  predsprobs<- catboost.predict(model,pool_test,prediction_type='Class')
+  #preds<-ifelse(predsprobs[,1]>0.5,0,1)
+  preds<-predsprobs
   return (preds)
 }
 
@@ -95,8 +93,8 @@ calculate_recall <-function(dataset){
   return(recall)
 }
 
-evaluate_model_test <- function(model, test_dataset_x, test_dataset_y, original_labels) {
-  preds<-get_predictions(model,test_dataset_x)
+evaluate_model_test <- function(model, test_dataset_x, test_dataset_y, pool_test, original_labels) {
+  preds<-get_predictions(model,pool_test)
   confmatrix<-confusionMatrix(data= as.factor(preds),reference = as.factor(test_dataset_y),positive = '1', mode="everything")
   print(confmatrix)
   result<-cbind(value=as.data.frame(confmatrix$byClass) %>% rownames_to_column())
@@ -107,15 +105,19 @@ evaluate_model_test <- function(model, test_dataset_x, test_dataset_y, original_
 }
 
 #### MAIN 
-#### MAIN 
 
 option_list <- list(
   make_option("--experimenttag", action="store", type="character", default="default-experiment", help = "Set experiment tag id "),
   make_option("--maxlen", action="store", type="numeric", default=45, help = "Set the maximun length of the seq  considered"),
   make_option("--upsample", action="store_true", help = "Apply oversampling to  train dataset",default=FALSE),
   make_option("--downsample", action="store_true", help = "Apply downsampling to  train dataset",default=FALSE),
-  make_option("--datafile", action="store", type="character", help = "A file to load dataset from", default = "ctu19subs.csv")
+  make_option("--datafile", action="store", type="character", help = "A file to load dataset from", default = "train.csv"),
+  make_option("--testfile", action="store", type="character", help = "A file to load dataset from", default = "test.csv")
 )
+
+  
+
+
 opt <- parse_args(OptionParser(option_list=option_list))
 
 print(opt)
@@ -124,44 +126,58 @@ datasetfile=opt$datafile
 results_dir='../dga-wb-r/results/'
 
 
+
+
 dataset<-read_valid_csv(datasetfile)
 if(is.null(dataset)){
   message("Error!")
   quit()
 }
-
 dindex<-train_test_sample(dataset,0.7)
 train_dataset<-dataset[dindex,]
 test_dataset<-dataset[-dindex,]
 
+
 if (opt$upsample){
   train_dataset<- caret::upSample(x=train_dataset[,c(1,3)], y=as.factor(train_dataset$class),list = F,yname = "class") 
   train_dataset<-train_dataset[,c(1,3,2)]
-  print(train_dataset %>% group_by(class) %>% summarise(n=n()))
   print(train_dataset %>% head(5))
-  quit()
 }
 
 if (opt$downsample){
   train_dataset<- caret::downSample(x=train_dataset[,c(1,3)], y=as.factor(train_dataset$class),list = F,yname = "class") 
   train_dataset<-train_dataset[,c(1,3,2)]
-  print(train_dataset %>% group_by(class) %>% summarise(n=n()))
   print(train_dataset %>% head(5))
-  quit()
 }
-
 
 dataset_train_vectorized<-vectorize_seq(train_dataset,opt$maxlen)
 dataset_test_vectorized<-vectorize_seq(test_dataset,opt$maxlen)
 print("[] Creating model and evaluating model on test ")
-rfModel<-randomForest(x= dataset_train_vectorized %>% select(-modelsize,-label,-class),y=dataset_train_vectorized$class,mtry = 2)
 
-#rfpreds<-predict(rfModel,dataset_test_vectorized %>% select(-modelsize,-label,-class), type="prob")
-#confusionMatrix(reference = as.factor(dataset_test_vectorized$class),data=as.factor(ifelse(rfpreds[,1]>0.5,0,1)),positive = "1", mode="everything" )
-#print("****-----****")
-results<-evaluate_model_test(rfModel,test_dataset_x = dataset_test_vectorized %>% select(-modelsize,-label,-class),
-                             test_dataset_y = dataset_test_vectorized$class,original_labels = dataset_test_vectorized$label)
+dataset_train_vectorized_x<-dataset_train_vectorized %>% select(-class, -label, -modelsize)
+dataset_train_vectorized_y<-dataset_train_vectorized$class
+
+dataset_test_vectorized_x<-dataset_test_vectorized %>% select(-class, -label, -modelsize)
+dataset_test_vectorized_y<-dataset_test_vectorized$class
+
+pool_train <- catboost.load_pool( dataset_train_vectorized_x, 
+                                  label = as.numeric(dataset_train_vectorized_y))
+
+pool_test <- catboost.load_pool( dataset_test_vectorized_x, 
+                                  label = as.numeric(dataset_test_vectorized_y)) 
+
+fit_params <- list(iterations=100,
+                   loss_function = 'Logloss',
+                   task_type = 'CPU')
+
+rfModel<-  catboost.train(pool_train, params = fit_params)
+
+results<-evaluate_model_test(rfModel,
+                             test_dataset_x = dataset_test_vectorized_x, 
+                             test_dataset_y = dataset_test_vectorized_y, 
+                             pool_test = pool_test,
+                             original_labels = dataset_test_vectorized$label)
 
 print("[] Saving results ")
-write_csv(results$result,col_names = T,path=paste(results_dir,"results_test_rf",opt$experimenttag,".csv",sep=""))
-write_csv(results$resultperclass,col_names = T,path=paste(results_dir,"results_per_subclass_test_rf",opt$experimenttag,".csv",sep=""))
+write_csv(results$result,col_names = T,path=paste(results_dir,"results_test_catboost_",opt$experimenttag,".csv",sep=""))
+write_csv(results$resultperclass,col_names = T,path=paste(results_dir,"results_per_subclass_test_catboost_",opt$experimenttag,".csv",sep=""))
